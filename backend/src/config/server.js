7fad56d435d764228ab7b237a62d740a0a8556bc
Leonardo Lima <leonardoresendelima@gmail.com>
2019-06-04 09:59:12 -0300

    first commit

---
+++ backend/src/config/server.js
@@ -0,0 +1,14 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+const port = 3003
+
+server.listen(port, () => {
+    console.log(`Servidor Rodando na porta ${port}`)
+})
+
+
